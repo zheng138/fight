7bc71d3235274c879d8051637a71180734ad226d
zheng138 <1479660675@qq.com>
2020-03-04 16:19:44 +0800

    基础题库-列表展示-数据搜索

--- src/module-hmmm/pages/questions.vue
+++ src/module-hmmm/pages/questions.vue
@@ -54,13 +54,20 @@
         <el-row :gutter="20">
           <el-col :span="6">
             城市:
-            <el-select style="width:85px" v-model="searchForm.province" placeholder="选城市">
+            <el-select
+              style="width:85px"
+              v-model="searchForm.province"
+              placeholder="选城市"
+              @change="getCitys(searchForm.province)"
+            >
               <!-- v-for='item in data成员/methods成员' -->
               <!-- methods成员在模板中也可以使用，注意设置( )即可
               {{provin()}}-->
               <el-option v-for="item in provin()" :key="item" :value="item" :label="item"></el-option>
             </el-select>
-            <el-select style="width:85px" v-model="searchForm.citys" placeholder="选地区"></el-select>
+            <el-select style="width:85px" v-model="searchForm.city" placeholder="选地区">
+              <el-option v-for="item in  cityList" :key="item" :value="item" :label="item"></el-option>
+            </el-select>
           </el-col>
           <!-- -------------------------------------- 关键字--------------------------------------------------------- -->
           <el-col :span="6">
@@ -84,7 +91,7 @@
             方向:
             <el-select class="wd" v-model="searchForm.direction" placeholder="请选择">
               <!-- <el-option
-                v-for="item in directionList"
+                v-for="item in directionList"                         
                 :key="item.value"
                 :value="item.value"
                 :label="item.label"
@@ -123,12 +130,37 @@
             <el-button type="primary">搜索</el-button>
           </el-col>
         </el-row>
+        <!-- 给table加了border就是给小表格加了边线了 -->
+        <el-table border :data="questionList" style="width:100%">
+          <el-table-column label="序号" type="index" width="60"></el-table-column>
+          <el-table-column label="试题编号" prop="number"></el-table-column>
+          <el-table-column label="学科" prop="subject"></el-table-column>
+          <!-- :formatter="questionTypeFMT",对列内容进行二次更新，注意不要设置()括号 -->
+          <!-- 有了此属性，当前列的内容就会受到questionTypeFMT的返回值决定了 -->
+          <el-table-column :formatter="questionTypeFMT" label="题型" prop="questionType"></el-table-column>
+          <el-table-column label="题干" prop="question"></el-table-column>
+          <el-table-column label="录入时间" width="170">
+            <!-- 通过插值表达式表现时间信息，并应用过滤器做格式转换 -->
+            <!-- 获得当前列的信息，作用域插槽应用 -->
+            <span slot-scope="stDate">{{stDate.row.addDate |parseTimeByString}}</span>
+          </el-table-column>
+          <el-table-column :formatter="difficultyTypeFMT" label="难度" prop="difficulty"></el-table-column>
+          <el-table-column label="录入人" prop="creator"></el-table-column>
+          <el-table-column label="操作" width="200">
+            <a href="#">预览</a>
+            <a href="#">修改</a>
+            <a href="#">删除</a>
+            <a href="#">加入精选</a>
+          </el-table-column>
+        </el-table>
       </el-card>
     </div>
   </div>
 </template>
 
 <script>
+// 导入基础题库列表展示的api
+import { list } from '@/api/hmmm/questions.js'
 // 导入城市 区县的api
 import { provinces, citys } from '@/api/hmmm/citys.js'
 // 输入二级目录的api
@@ -151,6 +183,10 @@ export default {
   name: 'QuestionsList',
   data() {
     return {
+      // 试题列表
+      questionList: [],
+      // 获得区县的下拉列表i
+      cityList: [],
       // 方向下拉列表
       directionList,
       // 录入二级目录下拉列表
@@ -167,6 +203,8 @@ export default {
       difficultyList,
       questionTypeList,
       searchForm: {
+        page: 1, // 默认页码
+        pagesize: 10, // 默认数据条数
         subjectID: '', // 学科
         difficulty: '', // 难度
         questionType: '', // 试题类型
@@ -182,6 +220,16 @@ export default {
       }
     }
   },
+  watch: {
+    // 搜索表单深度监听
+    searchForm: {
+      handler: function(newV, oldV) {
+        // 任何条件变化，都重新获得数据
+        this.getQuestionList()
+      },
+      deep: true
+    }
+  },
   created() {
     // 学科
     this.getSubjectIDList()
@@ -193,8 +241,59 @@ export default {
     this.getCatalogIDList()
     // // 城市
     // this.provin()
+    // 基础题库列表
+    this.getQuestionList()
   },
   methods: {
+    // 难度数字转汉字
+    difficultyTypeFMT(row, column, cellValue, index) {
+      // console.log(row)
+      // return row
+      // return row.answer
+      // return column
+      // console.log(column)
+      // return column.id
+      // return cellValue
+      // return row.questionType
+      // return this.questionTypeList[cellValue - 1].label
+
+      // 把cellValue的汉字内容进行显示
+      // return this.questionTypeList[cellValue - 1].label
+      return this.difficultyList[row.difficulty - 1].label
+    },
+    // 对题型 列 信息 进行二次更新操作
+    // row:代表当前每个数据项目记录信息的，是一个对象，{id:xxx,number:xx,difficult:xx,addDate:xx....}
+    //     可以通过这个row访问到当前任何数据项目记录信息，调用形式：row.id row.number
+    // column：可以获取记录的列的信息，一般不使用
+    // cellValue当前行当前列的数据项目记录信息，类似row.questionType
+    questionTypeFMT(row, column, cellValue, index) {
+      // console.log(row)
+      // return row
+      // return row.answer
+      // return column
+      // console.log(column)
+      // return column.id
+      // return cellValue
+      // return row.questionType
+      // return this.questionTypeList[cellValue - 1].label
+
+      // 把cellValue的汉字内容进行显示
+      // return this.questionTypeList[cellValue - 1].label
+      return this.questionTypeList[row.questionType - 1].label
+    },
+
+    // 获得基础列表
+    async getQuestionList() {
+      // 调用api,并传递参数,设置数据查询条件
+      let result = await list(this.searchForm)
+      this.questionList = result.data.items
+    },
+    // 获得 城市 信息
+    // 这个prame形参就代表被选中的省份信息
+    getCitys(pname) {
+      this.searchForm.city = '' // 清除之前选取好的城市
+      this.cityList = citys(pname)
+    },
     // 接收城市的函数
     provin: provinces,
     // 导入二级目录下拉列表的方法
@@ -230,6 +329,9 @@ export default {
 </script>
 
 <style scoped>
+.el-table {
+  margin-top: 25px;
+}
 .el-row {
   margin-bottom: 10px;
 }
